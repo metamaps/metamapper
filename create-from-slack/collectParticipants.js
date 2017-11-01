760a0744b0c2ba95ef1ccfa45345718e6ebebde5
Connor Turland <connorturland@gmail.com>
2017-10-31 22:17:05 -0400

    Delete collectParticipants.js

--- create-from-slack/collectParticipants.js
+++
@@ -1,8 +0,0 @@
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS
-const interactionText = require('./interactionText.js')
-
-function collectParticipants (context, cb) {
-
-}
-
-module.exports = collectParticipants
