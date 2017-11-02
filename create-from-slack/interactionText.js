1c6cce0c981097bca93f7eb7e0246d3a90b023bb
Connor Turland <connorturland@gmail.com>
2017-11-02 19:58:58 +0000

    remove todo

--- create-from-slack/interactionText.js
+++ create-from-slack/interactionText.js
@@ -40,7 +40,6 @@ Let’s start.`,
       },
       facilitatorSessionStarting: `The session is beginning. You will be updated here with information about how people are participating, and be able to guide the process.
 You can make announcements to all participants by typing “announce: “ followed by your message`,
-      // TODO: list participants, or number of participants
       channelSessionStarting: `There is an ${v.sessionType} session on *${v.title}* beginning with ${participantCount} participants.
 Results will be posted back here when the process is complete.`,
       participantSessionStarting: `You've been invited to an ${v.sessionType} session by <@${v.facilitator}> along with ${participantCount - 1} other participants.
